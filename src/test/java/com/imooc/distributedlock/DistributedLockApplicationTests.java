b0af2b4a8be668704fb0754198006a2549f34930
bizhengyong <bizhengyong_xy@163.com>
2021-03-02 19:50:49 +0800

    12-2-Redisson的使用

---
+++ src/test/java/com/imooc/distributedlock/DistributedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.distributedlock;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
